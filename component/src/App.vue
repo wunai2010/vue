ed12aa04e1267d6b49b460146c13a8308769c100
wangyuebai <wangyuebai@zhongan.io>
2021-04-26 21:06:33 +0800

    组件

---
+++ component/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <component :is="HelloWorld" @msg="你好"></component>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<MESSAGE msg="你好"></MESSAGE>
+	<MESSAGE msg="你好"></MESSAGE>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import MESSAGE from './components/h1.vue';
+
+var components1 = {HelloWorld,MESSAGE};
+export default {
+  name: 'App',
+  data:function(){
+	return {allComponents:['HelloWorld','MESSAGE']}
+  },
+  components:components1
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ component/src/components/h1.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="message">
+		<h1>{{msg}}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'MESSAGE',
+	props: {
+	msg: String
+	}
+}
+</script>
+
+<style>
+	.message{
+		color: #42B983;
+	}
+</style>
